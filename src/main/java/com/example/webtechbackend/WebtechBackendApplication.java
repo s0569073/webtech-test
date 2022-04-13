e72d4bdf3f0bfec7fb04b85a4e8bcd3845d4131a
rpank <s0569073@htw-berlin.de>
2022-04-13 14:05:48 +0200

    init

---
+++ src/main/java/com/example/webtechbackend/WebtechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webtechbackend/WebtechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
